382897fa7752b4327bd4ed1dac7cb32755373e3e
shane87 <eric.clothier@gmail.com>
2023-09-10 11:30:30 -0400

    document code, fix a derp in README

--- Tutorial2/student.cpp
+++ Tutorial2/student.cpp
@@ -1,10 +1,35 @@
-//student.cpp - define class student
+//student.cpp - defions for the 'student' class
+// student class members:
+//     private members:
+//          id:      integer holding student ID
+//          name:    string holding student name
+//          address: string holding student address
+//          gpa:     float holding student gpa, restricted to 0.0-4.0
+//          dob:     string holding student date of birth
+//     public members:
+//          student(): constructor - overloaded
+//          setId(): sets student id
+//          setName(): sets student name
+//          setAddress(): sets student address
+//          setGpa(): sets student gpa
+//          setDob(): sets student date of birth
+//          getId(): returns student id
+//          getName(): returns student name
+//          getAddress(): returns student address
+//          getGpa(): returns student gpa
+//          getDob(): returns student date of birth
 
+// BEGIN student.cpp
+// pull in the c++ std string library
 #include <string>
+// pull in the 'student.h' file with the class declarations
 #include "student.h"
 
+// let the compiler know we are using the 'std' namespace, see 'student.h'
 using namespace std;
 
+// default constructor:
+// instantiates the 'student' object and sets all values to default
 student::student()
 {
 	this->id = 0;
@@ -14,6 +39,10 @@ student::student()
 	this->dob = "";
 }
 
+// overloaded constructor:
+// instantiates the 'student' object and sets all values to those provided
+// performs basic check to make sure gpa parameter is within acceptable bounds
+// TODO: notify of gpa OOB error
 student::student(int id, string name, string address, float gpa, string dob)
 {
 	this->id = id;
@@ -26,24 +55,33 @@ student::student(int id, string name, string address, float gpa, string dob)
 	this->dob = dob;
 }
 
+// setId(int id):
+// sets student id to parameter provided
 bool student::setId(int id)
 {
 	this->id = id;
 	return true;
 }
 
+// setName(string name):
+// sets student name to parameter provided
 bool student::setName(string name)
 {
 	this->name = name;
 	return true;
 }
 
+// setAddress(string address):
+// sets student address to parameter provided
 bool student::setAdress(string address)
 {
 	this->address = address;
 	return true;
 }
 
+// setGpa(float gpa):
+// sets student gpa to parameter provided after check gpa is within bounds
+// TODO: notify of gpa OOB error
 bool student::setGpa(float gpa)
 {
 	if (gpa >= 0.0 && gpa <= 4.0)
@@ -57,32 +95,44 @@ bool student::setGpa(float gpa)
 	}
 }
 
+// setDob(string dob):
+// sets student dob to parameter provided
 bool student::setDob(string dob)
 {
 	this->dob = dob;
 	return true;
 }
 
+// getId():
+// returns student id
 int student::getId()
 {
 	return this->id;
 }
 
+// getName():
+// returns student name
 string student::getName()
 {
 	return this->name;
 }
 
+// getAddress():
+// returns student address
 string student::getAddress()
 {
 	return this->address;
 }
 
+// getGpa():
+// returns student gpa
 float student::getGpa()
 {
 	return this->gpa;
 }
 
+// getDob():
+// returns student date of birth
 string student::getDob()
 {
 	return this->dob;
--- Tutorial2/student.h
+++ Tutorial2/student.h
@@ -1,19 +1,53 @@
+// student.h - declarations for the 'student' class
+// student class members:
+//     private members:
+//          id:      integer holding student ID
+//          name:    string holding student name
+//          address: string holding student address
+//          gpa:     float holding student gpa, restricted to 0.0-4.0
+//          dob:     string holding student date of birth
+//     public members:
+//          student(): constructor - overloaded
+//          setId(): sets student id
+//          setName(): sets student name
+//          setAddress(): sets student address
+//          setGpa(): sets student gpa
+//          setDob(): sets student date of birth
+//          getId(): returns student id
+//          getName(): returns student name
+//          getAddress(): returns student address
+//          getGpa(): returns student gpa
+//          getDob(): returns student date of birth
+
+// BEGIN student.h
+// #ifndef/#define pair to prevent errors about multiple definitions of header
 #ifndef STUDENT_H
 #define STUDENT_H
 
+// pull in the c++ std string library
 #include <string>
 
+// all c++ std librarys belong to the 'std' namespace, so let the compiler know
+// we are using that namespace. if we were creating new namespaces we would
+// ommit this and then have to prepend all c++ std library function with std::
 using namespace std;
 
+// the meat and potatoes, as it were
+// start the declaration of the student class
 class student
 {
+	//unless otherwise specified by keywords, class members are private
+	//declare the private vars described above
 	int id;
 	string name;
 	string address;
 	float gpa;
 	string dob;
 
+	// use the 'public' keyword to declare the functions as public, for accessing
+	// class data from outside of the class
 public:
+	// declare the functions/methods described above
 	student();
 	student(int, string, string, float, string);
 	bool setId(int);
@@ -27,4 +61,6 @@ public:
 	float getGpa();
 	string getDob();
 };
+
+// don't forget the #endif to close out the #ifndef/#define pair from the top!
 #endif
